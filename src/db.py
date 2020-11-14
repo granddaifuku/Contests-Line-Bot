7ba3d540654886c0ee09f93d062538e07a65b313
granddaifuku <>
2020-11-14 11:17:33 +0900

    create functions for yukicoder

--- src/db.py
+++ src/db.py
@@ -1,11 +1,14 @@
 import os
 import psycopg2
 import psycopg2.extras
-import utils
+import atcoder
+import codeforces
+import yukicoder
 
 
 AT_TABLE = 'at_db'
 CF_TABLE = 'cf_db'
+YK_TABLE = 'yk_db'
 
 
 def get_connection():
@@ -17,7 +20,7 @@ def get_connection():
 def update_at_table():
     query = ''
     query += 'DELETE FROM {};'.format(AT_TABLE)
-    data = utils.format_at_info()
+    data = atcoder.format_at_info()
     for i in range(len(data)):
         query += 'INSERT INTO {0} (name, time, range) VALUES (\'{1}\', \'{2}\', \'{3}\');'.format(
             AT_TABLE, data[i]['name'], data[i]['time'], data[i]['range'])
@@ -27,7 +30,17 @@ def update_at_table():
 def update_cf_table():
     query = ''
     query += 'DELETE FROM {};'.format(CF_TABLE)
-    data = utils.format_cf_info()
+    data = codeforces.format_cf_info()
+    for i in range(len(data)):
+        query += 'INSERT INTO {0} (name, time) VALUES (\'{1}\', \'{2}\');'.format(
+            CF_TABLE, data[i]['name'], data[i]['time'])
+    execute(query)
+
+
+def update_yk_table():
+    query = ''
+    query += 'DELETE FROM {};'.format(YK_TABLE)
+    data = yukicoder.format_yk_info()
     for i in range(len(data)):
         query += 'INSERT INTO {0} (name, time) VALUES (\'{1}\', \'{2}\');'.format(
             CF_TABLE, data[i]['name'], data[i]['time'])
