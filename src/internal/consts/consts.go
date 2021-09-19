ac0368c53e5f80b8bce5899c5a4ef6760e1d423d
granddaifuku <daifukuformac@gmail.com>
2021-09-20 01:20:29 +0900

    define constants

---
+++ src/internal/consts/consts.go
@@ -0,0 +1,5 @@
+package consts
+
+const AtcoderURL = "https://atcoder.jp/contests/?lang=ja"
+const CodeforcesURL = "https://codeforces.com/api/contest.list?gym=false"
+const YukicoderURL = "https://yukicoder.me/api/v1/contest/future"
