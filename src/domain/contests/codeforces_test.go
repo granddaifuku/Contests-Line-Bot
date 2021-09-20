bdbd78e8d438e785824c0872888c7cf745ecd4ff
granddaifuku <daifukuformac@gmail.com>
2021-09-20 10:35:37 +0900

    fix test in codeforces_test to pass

--- src/domain/contests/codeforces_test.go
+++ src/domain/contests/codeforces_test.go
@@ -9,8 +9,8 @@ import (
 
 func TestNewCodeforcesInfo(t *testing.T) {
 	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
-	api := CodeforcesApi{Name: "Test Contests", Type: "ICPC", DurationSeconds: 3723, StartTimeSeconds: 1630454462}
-	expected := CodeforcesInfo{Name: "Test Contests", Type: "ICPC", StartTime: time.Date(2021, time.September, 1, 0, 1, 2, 0, time.UTC).In(jst), EndTime: time.Date(2021, time.September, 1, 1, 3, 5, 0, time.UTC).In(jst)}
+	api := CodeforcesApiResult{Name: "Test Contests", Phase: "Finished", DurationSeconds: 3723, StartTimeSeconds: 1630454462}
+	expected := CodeforcesInfo{Name: "Test Contests", StartTime: time.Date(2021, time.September, 1, 0, 1, 2, 0, time.UTC).In(jst), EndTime: time.Date(2021, time.September, 1, 1, 3, 5, 0, time.UTC).In(jst)}
 
 	actual := NewCodeforcesInfo(&api)
 	if diff := cmp.Diff(expected, actual); diff != "" {
