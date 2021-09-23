d175333be836a0ea814c406a7426f7ebbd730ba7
granddaifuku <daifukuformac@gmail.com>
2021-09-24 00:41:02 +0900

    add constants

--- src/internal/consts/consts.go
+++ src/internal/consts/consts.go
@@ -1,5 +1,9 @@
 package consts
 
-const AtcoderURL = "https://atcoder.jp/contests/?lang=ja"
-const CodeforcesURL = "https://codeforces.com/api/contest.list?gym=false"
-const YukicoderURL = "https://yukicoder.me/api/v1/contest/future"
+const (
+	AtcoderURL    = "https://atcoder.jp/contests/?lang=ja"
+	CodeforcesURL = "https://codeforces.com/api/contest.list?gym=false"
+	YukicoderURL  = "https://yukicoder.me/api/v1/contest/future"
+
+	TimeLayout = "2006-01-02 15:04:05"
+)
