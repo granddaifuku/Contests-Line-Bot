f3379d39fbbcfc7012e00bf90783a79291137cb9
granddaifuku <daifukuformac@gmail.com>
2021-09-27 16:45:49 +0900

    Use xerrors in domain/contests/atcoder

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/PuerkitoBio/goquery v1.7.1
 	github.com/google/go-cmp v0.5.6
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
 )
 
 require (
--- src/domain/contests/atcoder.go
+++ src/domain/contests/atcoder.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/granddaifuku/contest_line_bot/src/internal/consts"
+	"golang.org/x/xerrors"
 )
 
 var jst = time.FixedZone("Azia/Tokyo", 9*60*60)
@@ -27,22 +28,21 @@ func NewAtCoderInfo(
 	tz := "+0900"
 	// Delete the timezone suffix
 	if !strings.HasSuffix(start, tz) {
-		// TODO define errors in case there are different suffix
-		return *info, nil
+		return *info, xerrors.New("Error duration has no timezone suffix")
 	}
 	start = strings.TrimSuffix(start, tz)
 	dur := strings.Split(duration, ":") // Separate duratino to hours and minutes
 	startTime, err := time.ParseInLocation(consts.TimeLayout, start, jst)
 	if err != nil {
-		return *info, err
+		return *info, xerrors.Errorf("Error when Parsing Start Time: %w", err)
 	}
 	hours, err := strconv.Atoi(dur[0])
 	if err != nil {
-		return *info, err
+		return *info, xerrors.Errorf("Error when Converting String Hours to Int: %w", err)
 	}
 	minutes, err := strconv.Atoi(dur[1])
 	if err != nil {
-		return *info, err
+		return *info, xerrors.Errorf("Error when Converting String Minutes to Int: %w", err)
 	}
 	endTime := startTime.Add(time.Duration(hours)*time.Hour + time.Duration(minutes)*time.Minute)
 
