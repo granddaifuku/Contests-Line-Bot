b6fb7ce949d0d61e8d1d5584ff04eccae0580539
granddaifuku <daifukuformac@gmail.com>
2021-09-28 17:10:50 +0900

    remove unused constant

--- src/internal/consts/consts.go
+++ src/internal/consts/consts.go
@@ -4,6 +4,4 @@ const (
 	AtcoderURL    = "https://atcoder.jp/contests/?lang=ja"
 	CodeforcesURL = "https://codeforces.com/api/contest.list?gym=false"
 	YukicoderURL  = "https://yukicoder.me/api/v1/contest/future"
-
-	TimeLayout = "2006-01-02 15:04:05"
 )
