a503c49c5b0ad6e7b9763232e24b752519aa5992
granddaifuku <daifukuformac@gmail.com>
2021-10-02 16:53:49 +0900

    create a shell file to export envs for tests

---
+++ src/tests/envs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This shell script exports the environmental variables used in tests.
+export DB_USER_NAME="postgres"
+export DB_PASSWORD="password"
+export DB_PORT=5432
+export DB_NAME="test"
